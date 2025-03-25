c27a82475d611d6a7db0cebf30f65532dd7bf3d9
Erhahon Imafidon <mafiosco4@gmail.com>
2025-03-25 07:35:01 +0100

    created the postSlice

---
+++ src/features/posts/postSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export interface PostSliceState {
+    id: string;
+    title: string;
+    content: string;
+}
+
+const initialState: PostSliceState[] = [
+    {
+        id: '1',
+        title: 'Learning Redux Toolkit',
+        content: 'Redux Toolkit is the recommended way to write Redux logic.',
+    },
+
+    {
+        id: '2',
+        title: 'Slices',
+        content: 'The more I slice, the more I learn about Redux.',
+    },
+];
+
+const postSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {},
+});
